3c9b37e7137449dde70d211b8f44d4062b722ac7
Anton Zimin <zimkaa87@gmail.com>
2024-03-09 00:24:11 +0500

    fix: comment
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- src/application/inventory.py
+++ src/application/inventory.py
@@ -25,7 +25,7 @@ def forms_view(loguru: FormKind) -> list[AnyComponent]:
     # text
     # """
     return demo_page(
-        # c.Markdown(text=markdown),  # noqa: ERA001
+        # c.Markdown(text=markdown),
         c.ServerLoad(
             path="/inventory/content/{kind}",
             load_trigger=PageEvent(name="change-form"),
@@ -35,37 +35,37 @@ def forms_view(loguru: FormKind) -> list[AnyComponent]:
 
 
 # class LoginForm(BaseModel):
-#     cookie: str = Field(title="Cookie", description="insert cookie")  # noqa: ERA001
-#     login: str = Field(title="Login", description="insert login")  # noqa: ERA001
-#     password: SecretStr = Field(description="insert password")  # noqa: ERA001
-#     proxy_ip: str = Field(title="Ip", description="insert ip")  # noqa: ERA001
-#     proxy_port: str = Field(title="Port", description="insert port")  # noqa: ERA001
-#     proxy_log: str = Field(title="proxy log", description="insert password")  # noqa: ERA001
-#     proxy_pass: SecretStr = Field(description="insert password")  # noqa: ERA001
+#     cookie: str = Field(title="Cookie", description="insert cookie")
+#     login: str = Field(title="Login", description="insert login")
+#     password: SecretStr = Field(description="insert password")
+#     proxy_ip: str = Field(title="Ip", description="insert ip")
+#     proxy_port: str = Field(title="Port", description="insert port")
+#     proxy_log: str = Field(title="proxy log", description="insert password")
+#     proxy_pass: SecretStr = Field(description="insert password")
 
 
 # def read_file(file_path: str) -> str:
 #     with Path(file_path).open() as file:
-#         return file.read()  # noqa: ERA001
+#         return file.read()
 
 
 @router.get("/content/{kind}", response_model=FastUI, response_model_exclude_none=True)
 def form_content(kind: FormKind) -> list[AnyComponent]:
-    # read_file("tests_files/result.txt")  # noqa: ERA001
+    # read_file("tests_files/result.txt")
     with Path("items.pkl").open("rb") as file:
         items = pickle.load(file)  # noqa: S301
-    # items = pickle.load(open("items.pkl", "rb"))  # noqa: ERA001
+    # items = pickle.load(open("items.pkl", "rb"))
     match kind:
         case "default":
             # return [
-            #     # c.ModelForm(model=LoginForm, display_mode="page", submit_url="/api/inventory/default"),  # noqa: ERA001, E501
-            #     c.Markdown(text=text),  # noqa: ERA001
+            #     # c.ModelForm(model=LoginForm, display_mode="page", submit_url="/api/inventory/default"),
+            #     c.Markdown(text=text),
             # ]  # noqa: ERA001, RUF100
             return demo_page(
                 c.Table(
                     data=items,
                     columns=[
-                        # DisplayLookup(field="name", on_click=GoToEvent(url="/table/users/{id}/")),  # noqa: ERA001
+                        # DisplayLookup(field="name", on_click=GoToEvent(url="/table/users/{id}/")),
                         DisplayLookup(field="name"),
                         DisplayLookup(field="count"),
                     ],
